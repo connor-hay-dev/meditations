5db20fd3088f140a3c1fee536c5e70d78bbc656c
ConnorCoderAcademy <13716@coderacademy.edu.au>
2022-12-07 20:21:53 +1100

    first commit, added meditations text, created variable for each chapter

---
+++ main.py
@@ -0,0 +1,16 @@
+import json
+
+from meditations_text import meditations
+
+meditations_book_1 = meditations["1"]
+meditations_book_2 = meditations["2"]
+meditations_book_3 = meditations["3"]
+meditations_book_4 = meditations["4"]
+meditations_book_5 = meditations["5"]
+meditations_book_6 = meditations["6"]
+meditations_book_7 = meditations["7"]
+meditations_book_8 = meditations["8"]
+meditations_book_9 = meditations["9"]
+meditations_book_10 = meditations["10"]
+meditations_book_11 = meditations["11"]
+meditations_book_12 = meditations["12"]
