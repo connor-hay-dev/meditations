6cf8903c39678d0fb0e3fe8ede76721ef322939e
ConnorCoderAcademy <13716@coderacademy.edu.au>
2022-12-15 17:54:36 +1100

    added random functionality to scramble order of questions and answer options to questions.

--- quiz_questions.py
+++ quiz_questions.py
@@ -1,35 +1,39 @@
-# questions for quiz game in a dictionary format
-
-name = input("What's your name? ")
+import random
 
 # a nice function that provides lower case letters which I have used to number the question options (a,b,c,d instead of (1,2,3,4).
 from string import ascii_lowercase
 
+number_of_questions_per_quiz = 5
 
-questions = { 
+quiz_questions = { 
     "What was Marcus Aurelius' occupation": [ "Emperor", "Slavemaster", "Financier", "Artisan"
     ],
     "Which school of philosphy is Marcus Aurelius famous in": [ "Stoicism", "Epicureanism", "Skepticism", "Buddhism"
     ],
     "What is the modern name of the country in which Marcus Aurelius was born": [ "Italy", "Spain", "Nigeria", "Portugal"
     ],
-
-
+    "How many books is Marcus Aurelius' meditations comprised of?: ": [ "12", "20", "15", "200"
+    ],
+    "Fill in the blank word in the following quote: \"Everything suits me that suits your ____, oh my universe.\"": [ "designs", "plans", "schemes", "desires"
+    ]
 }
+
+number_of_questions = min(number_of_questions_per_quiz, len(quiz_questions))
+questions = random.sample(list(quiz_questions.items()), k=number_of_questions_per_quiz)
 
 # lists question and answer options 
 num_correct = 0
-for num, (question, alt_answers) in enumerate(questions.items(), start=1):
+for num, (question, alt_answers) in enumerate(questions, start=1):
     print(f"\nQuestion {num}")
     # prints the question with a question mark so I do not have to remember to put them in when I make the question lol
     print(f"{question}?")
     correct_answer = alt_answers[0]
     # sorted() changes the answer order (orders alphabetically to shuffle the order)
-    numbered_answers = dict(zip(ascii_lowercase, sorted(alt_answers)))
+    numbered_answers = dict(zip(ascii_lowercase, random.sample(alt_answers, k=len(alt_answers))))
     # add a number for each question option (iterates through the for loop and number each option)
     for number, alternative in numbered_answers.items():
         print(f" {number}) {alternative}")
-   
+    # checks to see if user answer is the same as the options (a,b,c or d) and if not, prompts user to pick again
     while (answer_number := input("\nYour Choice?: ")) not in numbered_answers:
         print(f"Please pick between options a, b, c, or d")
 
@@ -42,6 +46,6 @@ for num, (question, alt_answers) in enumerate(questions.items(), start=1):
 
 print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 print(f"\nYou got {num_correct} answers correct out of {num} questions!")
-average = (num_correct/num)*100
-print(f"That is a success rate of ", round(average, 2), "%!")
+score_average = (num_correct/num)*100
+print(f"That is a success rate of ", round(score_average, 2), "%!")
 print("\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
